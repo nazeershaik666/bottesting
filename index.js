fc1cb4f2613ddebea9379be01e6a2326bf095862
nazeershaik666 <121153446+nazeershaik666@users.noreply.github.com>
2023-07-07 21:00:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const apiToken = '5914147662:AAEazL9O444TNO4dm-VxVWul4W-fqGsnHZM'
 // Configurations
 app.use(bodyParser.json());// Endpoints
 app.post('/', (req, res) => {
-     // console.log(req.body);
+ console.log(req.body);
  let chatId , sentMessage , messageId , edited = false
   if(req.body.message.document){
      chatId = req.body.message.chat.id;
      sentMessage = req.body.message.document.file_name;
      messageId = req.body.message.message_id;
      console.log(1)
-   //  console.log(1,chatId,sentMessage,messageId)
+     console.log(1,chatId,sentMessage,messageId)
   }else if(req.body.edited_message) {
     chatId = req.body.edited_message.chat.id 
     sentMessage = req.body.edited_message.text;
     messageId = req.body.edited_message.message_id;
     edited = true
     console.log(2)
-   // console.log(2,chatId,sentMessage,messageId)
+    console.log(2,chatId,sentMessage,messageId)
   }else{
   chatId = req.body.message.chat.id;
   messageId = req.body.message.message_id,
